a3efd41ac7722b4b549dfa3112c572308dfd393b
Juan David <juandavid@iMac-de-Juan.local>
2020-12-14 12:30:04 -0500

    Creacion de ruta con multiples applicartion properties

--- Spring-boot-Ejemplos/múltiples_archivosde_aplicaciones.propiedades/multiples-aplication-properties/src/main/java/com/juanmahecha9/multiples/aplication/properties/MultiplesAplicationPropertiesApplication.java
+++ Spring-boot-Ejemplos/múltiples_archivosde_aplicaciones.propiedades/multiples-aplication-properties/src/main/java/com/juanmahecha9/multiples/aplication/properties/MultiplesAplicationPropertiesApplication.java
@@ -3,8 +3,15 @@ package com.juanmahecha9.multiples.aplication.properties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RestController
 @SpringBootApplication
 public class MultiplesAplicationPropertiesApplication {
+
+    @RequestMapping("/")
+    String home() {
+        return "Juan David Mahecha Cruz";
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(MultiplesAplicationPropertiesApplication.class, args);
